7b07d4e3b66c8e62cbeef167557c264c7ad04b7e
josh_riang <sandinya1sampai8@gmail.com>
2026-05-03 20:49:42 +0700

    Updating Vercel Configuration & CORS Variable

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@press/data'],
-  allowedDevOrigins: ["f41b-43-252-145-206.ngrok-free.app"],
+  allowedDevOrigins: ["2003-43-252-145-206.ngrok-free.app"],
   async rewrites() {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:5000/api/v1/:path*'
+        destination: `${process.env.BACKEND_API_URL || 'http://localhost:5000/api/v1'}/:path*`
       }
     ];
   }
